cb3caa598f2b5e8ab7c8f4cd3446fe557471a2bb
DAM <DAM>
2016-10-18 13:41:53 +0200

    Primer commit

---
+++ src/principal.java
@@ -0,0 +1,28 @@
+
+
+import java.io.*;
+public class principal {
+
+	
+	
+	public static void main(String[] args)throws IOException {
+		//Creamos un objeto similar a scanner (BufferedReader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		//Declaramos una variable de tipo texto: String
+		String texto;
+		
+		//Pedimos introducir un texto, imprimiendo por pantalla la solicitud
+		System.out.print("Introduce un texto para ponerlo en mayúsculas: ");
+			//Se recoge la línea entera que escribimos
+			 texto=br.readLine();
+			 //Lo transformamos en mayúscula todo lo escrito
+			 	texto=texto.toUpperCase();
+			 
+			 	//ahora imprimimos el resultado
+			 System.out.println(texto);
+		
+		
+
+	}
+
+}
