d18131a554bbdc9b8b065df5f99648f6d1311481
DAM <DAM>
2016-10-18 14:32:26 +0200

    Hemos cambiado una tontería de entrada y clase persona

---
+++ src/Persona.java
@@ -0,0 +1,9 @@
+
+public class Persona {
+	
+	
+	String nombre, apellidos;
+	float peso;
+	
+	
+}
--- src/principal.java
+++ src/principal.java
@@ -6,13 +6,15 @@ public class principal {
 	
 	
 	public static void main(String[] args)throws IOException {
+		//Guarda remotamente git
+		
 		//Creamos un objeto similar a scanner (BufferedReader
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		//Declaramos una variable de tipo texto: String
 		String texto;
 		
 		//Pedimos introducir un texto, imprimiendo por pantalla la solicitud
-		System.out.print("Introduce un texto para ponerlo en mayúsculas: ");
+		System.out.print("Escribe algo: ");
 			//Se recoge la línea entera que escribimos
 			 texto=br.readLine();
 			 //Lo transformamos en mayúscula todo lo escrito
